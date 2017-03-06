fc1295e514ddd3c532802931d7eea593df30abc2
Fernie <ericf2023@gmail.com>
2017-03-05 22:17:27 -0500

    tests

---
+++ tests/unit/server.spec.ts
@@ -0,0 +1,17 @@
+/* global describe it */
+import { Server } from "./../../src/server";
+import { expect } from "chai";
+
+describe("AppServer", () => {
+  let appServer = new Server();
+
+  before("start", () => {
+    return appServer.start();
+  });
+
+
+  it( "is of type server", () => {
+    expect( appServer ).to.exist;
+    expect( appServer ).to.have.property( "start" );
+  });
+});
